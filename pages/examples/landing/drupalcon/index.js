b38ad26cad9b036c0d4699a4b50b2d8801bb5b18
alex-moreno <alejandro.moreno@tdo.es>
2022-09-05 18:20:29 +0200

    Pointed end points to new backend.

--- pages/examples/landing/drupalcon/index.js
+++ pages/examples/landing/drupalcon/index.js
@@ -38,15 +38,6 @@ export default function LandingSSRExample({
     </div>
   </div>
 
-
-  <ArticleGrid
-            data={articles}
-            contentType="articles"
-            multiLanguage={multiLanguage}
-          />
-
-
-
   <div class="row mb-2">
 
     {articles ? (
@@ -70,10 +61,13 @@ export default function LandingSSRExample({
                 >
                   <a className="font-normal underline">Read more →</a>
                 </Link>
-                  
+                img:{node.img} {node.img}
                 </div>
                 <div class="col-auto d-none d-lg-block">
-                  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#55595c"/>
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
         
                 </div>
               </div>
